819aa0801d42e706efb419de80ce3525930d3805
Joyee Cheung <joyeec9h3@gmail.com>
2015-02-20 00:16:50 +0800

    start working on styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var livereload = require('gulp-livereload');
 var ch_locals = require('./ch_locals.js');
+var path = require('path');
 
+
+// Jade to html
 gulp.task('jade', function() {
   return gulp.src('./src/*.jade')
     .pipe(jade({
@@ -13,17 +16,27 @@ gulp.task('jade', function() {
     .pipe(livereload());
 });
 
+// less to css
 gulp.task('less', function() {
-  return gulp.src('./src/*.less')
-    .pipe(less())
+  return gulp.src('./src/index.less')
+    .pipe(less({paths: [ path.join(__dirname, 'src', 'includes')]}))
     .pipe(gulp.dest('./dist/'))
     .pipe(livereload());
+});
+
+gulp.task('static', function() {
+  return gulp.src('./static/**/', {
+    base: 'static'
+  })
+  .pipe(gulp.dest('./dist/static/'))
+  .pipe(livereload());
 });
 
 gulp.task('watch', function() {
   livereload.listen();
   gulp.watch('./src/**/*.jade', ['jade']);
-  gulp.watch('./src/*.less', ['less']);
+  gulp.watch('./src/**/*.less', ['less']);
+  gulp.watch('./resume.json', ['jade']);
 });
 
-gulp.task('default', ['jade', 'less', 'watch']);
+gulp.task('default', ['jade', 'less', 'static', 'watch']);
