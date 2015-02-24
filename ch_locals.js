f1e5693e670a8bb50047e00e9de9404fb2c2a46c
Joyee Cheung <joyeec9h3@gmail.com>
2015-02-25 01:10:22 +0800

    better semantics

--- ch_locals.js
+++ ch_locals.js
@@ -20,6 +20,24 @@ module.exports = {
         "repo": "源代码",
         "pdf": "PDF 下载"
     },
+    section_names: {
+        "education": {
+            "ch": "教育经历",
+            "en": "Education"
+        },
+        "language": {
+            "ch": "语言水平",
+            "en": "Language"
+        },
+        "experience": {
+            "ch": "项目与工作经验",
+            "en": "Experience"
+        },
+        "skills": {
+            "ch": "技能",
+            "en": "Skills"
+        }
+    },
     highlight: highlight
 };
 
