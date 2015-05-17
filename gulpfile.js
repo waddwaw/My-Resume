d90ed0fd3764517d6aafd9cde349cedb33c62626
Joyee Cheung <joyeec9h3@gmail.com>
2015-05-17 18:33:05 +0800

    markdown to pages

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,13 @@ var gulp = require('gulp');
 var jade = require('gulp-jade');
 var less = require('gulp-less');
 var markdown = require('gulp-markdown');
-var connect = require('gulp-connect');
+var layout = require('gulp-layout');
+var livereload = require('gulp-livereload');
 var deploy = require('gulp-gh-pages');
+var sourcemaps = require('gulp-sourcemaps');
 var path = require('path');
+var http = require('http');
+var st = require('st');
 
 // Jade to html
 gulp.task('jade', function() {
@@ -13,18 +17,30 @@ gulp.task('jade', function() {
       locals: require('./ch_locals.js')
     }))
     .pipe(gulp.dest('./dist/'))
-    .pipe(connect.reload());
+    .pipe(livereload());
 });
 
 // less to css
 gulp.task('less', function() {
-  return gulp.src('./src/less/index.less')
+  gulp.src('./src/less/questions.less')
+    .pipe(sourcemaps.init())
     .pipe(less({
       paths: [path.join(__dirname, 'src', 'less', 'includes'),
               path.join(__dirname, 'src', 'less', 'components')]
     }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/questions/'))
+    .pipe(livereload());
+
+  gulp.src('./src/less/index.less')
+    .pipe(sourcemaps.init())
+    .pipe(less({
+      paths: [path.join(__dirname, 'src', 'less', 'includes'),
+              path.join(__dirname, 'src', 'less', 'components')]
+    }))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist/'))
-    .pipe(connect.reload());
+    .pipe(livereload());
 });
 
 // Static assets
@@ -33,7 +49,7 @@ gulp.task('static', function() {
       base: 'static'
     })
     .pipe(gulp.dest('./dist/static/'))
-    .pipe(connect.reload());
+    .pipe(livereload());
 });
 
 // Demos
@@ -42,18 +58,31 @@ gulp.task('demo', function() {
       base: './interview/demo'
     })
     .pipe(gulp.dest('./dist/demo/'))
-    .pipe(connect.reload());
+    .pipe(livereload());
 });
 
 // questions
 gulp.task('questions', function() {
-  return gulp.src('./interview/questions/**.md')
-    .pipe(markdown())
+  gulp.src('./node_modules/highlight.js/styles/tomorrow.css')
+    .pipe(gulp.dest('./dist/questions/'));
+
+  gulp.src('./interview/questions/**.md')
+    .pipe(markdown({
+      highlight: function (code) {
+        return require('highlight.js').highlightAuto(code).value;
+      }
+    }))
+    .pipe(layout({
+      layout: './src/jade/layout/questions-layout.jade'
+    }))
     .pipe(gulp.dest('./dist/questions/'))
-    .pipe(connect.reload());
+    .pipe(livereload());
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', ['server'], function() {
+  livereload.listen({ basePath: 'dist' });
+  gulp.watch('./interview/demo/**/*', ['demo']);
+  gulp.watch('./interview/questions/**/*', ['questions']);
   gulp.watch(['./src/**/*.jade', './resume.json', './ch_locals.js'], ['jade']);
   gulp.watch('./src/**/*.less', ['less']);
   gulp.watch('./resume.json', ['jade']);
@@ -61,13 +90,10 @@ gulp.task('watch', function() {
 
 gulp.task('build', ['jade', 'less', 'static', 'demo', 'questions']);
 
-gulp.task('connect', ['build'], function() {
-  connect.server({
-    root: 'dist',
-    port: 8000,
-    livereload: true,
-    fallback: "index.html"
-  });
+gulp.task('server', ['build'], function(done) {
+  http.createServer(
+    st({ path: __dirname + '/dist', index: 'index.html', cache: false })
+  ).listen(8000, done);
 });
 
 gulp.task('deploy', ['build'], function() {
@@ -75,4 +101,4 @@ gulp.task('deploy', ['build'], function() {
     .pipe(deploy());
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['server', 'watch']);
---
+++ interview/questions/cross-domain-ajax.md
@@ -0,0 +1,17 @@
+## Cross-Origin Resource Sharing
+
+For IE 8 & 9:
+
+```javascrpit
+var xdr = new XDomainRequest();
+```
+
+
+
+## Image Pings
+
+## JSONP
+
+## postMessage
+
+## document.domain
