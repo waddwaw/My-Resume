6b7b8935484fbb0005947e653d8cf807a6ba3740
Joyee Cheung <joyeec9h3@gmail.com>
2015-06-14 02:25:58 +0800

    DRY out gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,89 +5,99 @@ var path = require('path');
 var http = require('http');
 var st = require('st');
 
-function highlight(str) {
-  return str.replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
-    .replace(/`(.+?)`/g, '<strong>$1</strong>');
-}
-
-// Jade to html
-gulp.task('jade', function() {
-  var resume_data = require('./resume.json');
-  var locals = require('./i18n/' + resume_data.data_lang
-                        + '/dict.js');
-  for (var item in resume_data) {
-    locals[item] = resume_data[item];
+/******************* Jade to html ***********/
+function getLocals() {
+  var resumeData = require('./resume.json');
+  var locals = require('./i18n/' +
+                       resumeData.data_lang + '/dict.js');
+  for (var item in resumeData) {
+    locals[item] = resumeData[item];
   }
 
-  locals.highlight = highlight;
+  locals.highlight = function highlight(str) {
+    return str.replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
+      .replace(/`(.+?)`/g, '<strong>$1</strong>');
+  }
 
+  return locals;
+}
+
+gulp.task('jade', function() {
   return gulp.src('./src/jade/index.jade')
     .pipe(plugins.jade({
-      locals: locals
+      locals: getLocals()
     }))
     .pipe(gulp.dest('./dist/'))
     .pipe(plugins.livereload());
 });
 
-// less to css
+/************* less to css  ********************/
+var lessPath = [path.join(__dirname, 'src', 'less', 'includes'),
+                path.join(__dirname, 'src', 'less', 'components')];
+
+function less2css(srcPath, destPath, debug) {
+  if(debug) {
+    return gulp.src(srcPath)
+      .pipe(plugins.less({ paths: lessPath }))
+      .pipe(plugins.minifyCss({ compatibility: 'ie9' }))
+      .pipe(gulp.dest(destPath));
+  } else {
+    return gulp.src(srcPath)
+      .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.less({ paths: lessPath }))
+      .pipe(plugins.sourcemaps.write())
+      .pipe(gulp.dest(destPath))
+      .pipe(plugins.livereload());
+  }
+}
+
 gulp.task('less', function() {
-  gulp.src('./src/less/index.less')
-    .pipe(plugins.less({
-      paths: [path.join(__dirname, 'src', 'less', 'includes'),
-              path.join(__dirname, 'src', 'less', 'components')]
-    }))
-    .pipe(plugins.minifyCss({compatibility: 'ie9'}))
-    .pipe(gulp.dest('./dist/'))
+  less2css('./src/less/index.less', './dist/');
 });
 
 gulp.task('less-debug', function() {
-  gulp.src('./src/less/index.less')
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.less({
-      paths: [path.join(__dirname, 'src', 'less', 'includes'),
-              path.join(__dirname, 'src', 'less', 'components')]
-    }))
-    .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest('./dist/'))
-    .pipe(plugins.livereload());
+  less2css('./src/less/index.less', './dist/', true);
 });
 
-// Static assets
+/************** Static assets **************/
 gulp.task('static', function() {
-  return gulp.src('./static/**/*', {
-      base: 'static'
-    })
+  return gulp.src('./static/**/*', { base: 'static' })
     .pipe(gulp.dest('./dist/static/'))
     .pipe(plugins.livereload());
 });
 
-
+/****************** Watch ****************/
 gulp.task('watch', ['server'], function() {
-  plugins.livereload.listen({
-    basePath: 'dist'
-  });
-  gulp.watch(['./src/**/*.jade', './resume.json', './ch_locals.js'], ['jade']);
+  plugins.livereload.listen({ basePath: 'dist' });
+  gulp.watch(['./src/**/*.jade', './resume.json',
+              './i18n/**/*.js'], ['jade']);
   gulp.watch('./src/**/*.less', ['less']);
-  gulp.watch('./resume.json', ['jade']);
 });
 
+/****************** Build ****************/
 gulp.task('build', ['jade', 'less-debug', 'static']);
 gulp.task('build-for-deploy', ['jade', 'less', 'static']);
 
+/****************** Server ****************/
 function server(done) {
   http.createServer(
-    st({ path: __dirname + '/dist', index: 'index.html', cache: false })
+    st({
+      path: __dirname + '/dist',
+      index: 'index.html',
+      cache: false
+    })
   ).listen(8000, done);
   console.log("preview listening on http://localhost:8000");
 }
 
 gulp.task('server', ['build'], server);
-
 gulp.task('preview', ['build-for-deploy'], server);
 
+/****************** Deploy ****************/
 gulp.task('deploy', ['build-for-deploy'], function() {
   return gulp.src('./dist/**/*')
     .pipe(plugins.ghPages());
 });
 
+/****************** Default ****************/
 gulp.task('default', ['server', 'watch']);
