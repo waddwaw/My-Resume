8513956bf44396203df888a9f0c9c4bda8b4e3b1
Joyee Cheung <joyeec9h3@gmail.com>
2015-08-16 21:46:21 +0800

    use gulp-live-server, delete require cache, update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,40 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
 var path = require('path');
-var http = require('http');
-var st = require('st');
+var gls = require('gulp-live-server');
+
+var server = gls.static('dist', 8000);
+
+/**************** Utility **********************/
+function highlight(str) {
+  return str.replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
+    .replace(/`(.+?)`/g, '<strong>$1</strong>');
+}
 
 /******************* Jade to html ***********/
 function getLocals() {
   var resumeData = require('./resume.json');
-  var locals = require('./i18n/' +
-                       resumeData.data_lang + '/dict.js');
+  var localePath = './i18n/' + resumeData.data_lang + '/dict.js';
+  var locals = require(localePath);
+
+  // remove cache
+  delete require.cache[require.resolve('./resume.json')];
+  delete require.cache[require.resolve(localePath)];
+
+  // integrate the context
   for (var item in resumeData) {
     locals[item] = resumeData[item];
   }
 
-  locals.highlight = function highlight(str) {
-    return str.replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
-      .replace(/`(.+?)`/g, '<strong>$1</strong>');
-  }
+  locals.highlight = highlight;
 
   return locals;
 }
 
 gulp.task('jade', function() {
   return gulp.src('./src/jade/index.jade')
-    .pipe(plugins.debug())
-    .pipe(plugins.jade({
-      locals: getLocals()
-    }))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(plugins.livereload());
+    .pipe(plugins.jade({ locals: getLocals() }))
+    .pipe(gulp.dest('./dist/'));
 });
 
 /************* less to css  ********************/
@@ -40,43 +46,42 @@ function less2css(srcPath, destPath, debug) {
   if(!debug) {
     return gulp.src(srcPath)
       .pipe(plugins.less({ paths: lessPath }))
-      .pipe(plugins.debug())
       .pipe(plugins.minifyCss({ compatibility: 'ie9' }))
       .pipe(gulp.dest(destPath));
   } else {
     return gulp.src(srcPath)
-      .pipe(plugins.debug())
       .pipe(plugins.sourcemaps.init())
       .pipe(plugins.less({ paths: lessPath }))
       .pipe(plugins.sourcemaps.write())
-      .pipe(gulp.dest(destPath))
-      .pipe(plugins.livereload());
+      .pipe(gulp.dest(destPath));
   }
 }
 
 gulp.task('less', function() {
+  less2css('./src/less/questions.less', './dist/questions/');
   less2css('./src/less/index.less', './dist/');
 });
 
 gulp.task('less-debug', function() {
+  less2css('./src/less/questions.less', './dist/questions/', true);
   less2css('./src/less/index.less', './dist/', true);
 });
 
 /************** Static assets **************/
 gulp.task('static', function() {
   return gulp.src('./static/**/*', { base: 'static' })
-    .pipe(plugins.changed('./dist/static/'))
-    .pipe(plugins.debug())
-    .pipe(gulp.dest('./dist/static/'))
-    .pipe(plugins.livereload());
+    .pipe(gulp.dest('./dist/static/'));
 });
 
 /****************** Watch ****************/
 gulp.task('watch', ['server'], function() {
-  plugins.livereload.listen({ basePath: 'dist' });
-  gulp.watch(['./src/**/*.jade', './resume.json',
-              './i18n/**/*.js'], ['jade']);
-  gulp.watch('./src/**/*.less', ['less']);
+  gulp.watch(['./src/**/*.jade', './resume.json', './i18n/**/*.js'],
+             ['jade']);
+  gulp.watch('./static/**/*', ['static']);
+  gulp.watch('./src/**/*.less', ['less-debug']);
+  gulp.watch('./dist/**/*', function() {
+    server.notify.apply(server, arguments);
+  });
 });
 
 /****************** Build ****************/
@@ -84,19 +89,12 @@ gulp.task('build', ['jade', 'less-debug', 'static']);
 gulp.task('build-for-deploy', ['jade', 'less', 'static']);
 
 /****************** Server ****************/
-function server(done) {
-  http.createServer(
-    st({
-      path: __dirname + '/dist',
-      index: 'index.html',
-      cache: false
-    })
-  ).listen(8000, done);
-  console.log("preview listening on http://localhost:8000");
-}
+gulp.task('serve', function () {
+  server.start();
+});
 
-gulp.task('server', ['build'], server);
-gulp.task('preview', ['build-for-deploy'], server);
+gulp.task('server', ['build', 'serve']);
+gulp.task('preview', ['build-for-deploy', 'serve']);
 
 /****************** Deploy ****************/
 gulp.task('deploy', ['build-for-deploy'], function() {
